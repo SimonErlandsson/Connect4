11d8216e8aca5a6cef282cf36ccbde35ebba10ba
Simon Erlandsson <kontakt@simonerlandsson.se>
2022-02-15 23:36:29 +0100

    Added UI, game now works

---
+++ connect_ai.py
@@ -0,0 +1,146 @@
+from json.encoder import INFINITY
+import random
+import requests
+import numpy as np
+import argparse
+import sys
+
+# -----GAME LOGIC-----
+
+
+def get_available_actions(state):
+    available = []
+    for i in range(len(state[0])):
+        if (state[0][i] == 0):
+            available.append(i)
+    random.shuffle(available)
+    return available
+
+
+def apply_action(state, action, player):
+    new_state = np.copy(state)
+    row = 0
+    while row < len(state) - 1 and state[row + 1][action] == 0:
+        row += 1
+    new_state[row][action] = player
+    return new_state
+
+
+# ------EVALUATION FUNCTION & CHUNKING-------
+def utility(state):
+    score = 0
+    rows = len(state)
+    cols = len(state[0])
+    for row in range(rows):
+        for col in range(cols):
+            pos = (row, col)
+
+            # Check chunks of four
+            for chunk in get_chunks(state, pos, 4):
+                score += evaluate_chunk(chunk, 1) - evaluate_chunk(chunk, -1)
+
+    return score
+
+
+def evaluate_chunk(chunk, player):
+    if chunk.count(player) == 2 and chunk.count(0) == 2:  # two
+        return 1
+    elif chunk.count(player) == 3 and chunk.count(0) == 1:  # three
+        return 5
+    elif chunk.count(player) == 4:  # winning
+        return 1000
+    return 0
+
+
+def is_anyone_winning(state):
+    rows = len(state)
+    cols = len(state[0])
+    for row in range(rows):
+        for col in range(cols):
+            pos = (row, col)
+            for chunk in get_chunks(state, pos, 4):
+                if chunk.count(1) == 4 or chunk.count(-1) == 4:
+                    return True
+    return False
+
+
+def get_who_is_winning(state):
+    rows = len(state)
+    cols = len(state[0])
+    for row in range(rows):
+        for col in range(cols):
+            pos = (row, col)
+            for chunk in get_chunks(state, pos, 4):
+                if chunk.count(1) == 4:
+                    return 1
+                elif chunk.count(-1) == 4:
+                    return -1
+    return 0
+
+
+def get_chunks(state, pos, size):
+    chunks = []
+    max_row = len(state) - 1
+    max_col = len(state[0]) - 1
+
+    # Vertical (up-down)
+    if pos[0] + (size - 1) <= max_row:
+        chunks.append([state[pos[0] + i][pos[1]] for i in range(size)])
+
+    # Horizontal (left-right)
+    if pos[1] + (size - 1) <= max_col:
+        chunks.append([state[pos[0]][pos[1] + i] for i in range(size)])
+
+    # Diagonal \ (down right)
+    if pos[0] + (size - 1) <= max_row and pos[1] + (size - 1) <= max_col:
+        chunks.append([state[pos[0] + i][pos[1] + i] for i in range(size)])
+
+    # Anti-diagonal / (down left)
+    if pos[0] + (size - 1) <= max_row and pos[1] - (size - 1) >= 0:
+        chunks.append([state[pos[0] + i][pos[1] - i] for i in range(size)])
+
+    return chunks
+
+
+# --------MIN-MAX----------
+MAX_DEPTH = 5
+
+
+def min_max_value(state):
+    max_util = -INFINITY
+    max_a = -1
+    for a in get_available_actions(state):
+        new_state = apply_action(state, a, 1)
+        score = min_value(new_state, -INFINITY, INFINITY, 1)
+        if (score > max_util):
+            max_util = score
+            max_a = a
+    return max_a
+
+
+def max_value(state, alpha, beta, depth):
+    if (depth == MAX_DEPTH or is_anyone_winning(state)):
+        return utility(state)
+
+    v = -INFINITY
+    for a in get_available_actions(state):
+        new_state = apply_action(state, a, 1)
+        v = max(v, min_value(new_state, alpha, beta, depth + 1))
+        if v >= beta:
+            return v
+        alpha = max(alpha, v)
+    return v
+
+
+def min_value(state, alpha, beta, depth):
+    if (depth == MAX_DEPTH or is_anyone_winning(state)):
+        return utility(state)
+
+    v = INFINITY
+    for a in get_available_actions(state):
+        new_state = apply_action(state, a, -1)
+        v = min(v, max_value(new_state, alpha, beta, depth + 1))
+        if v <= alpha:
+            return v
+        beta = min(beta, v)
+    return v
---
+++ main.py
@@ -0,0 +1,99 @@
+import numpy as np
+import connect_ai
+import pygame
+pygame.init()
+pygame.font.init()
+
+# GAME STATICS
+BLOCK_SIZE = 100
+BOARD_SIZE = (6, 7)
+BORDER_SIZE = 1
+
+# COLORS
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+GAME_FONT = pygame.font.SysFont('Comic Sans MS', 60)
+
+
+def is_finished(board):
+    winner = connect_ai.get_who_is_winning(board)
+    if len(connect_ai.get_available_actions(board)) == 0:
+        print("Draw!")
+        return True, "It's a draw!", WHITE
+    elif winner != 0:
+        if winner == -1:
+            return True, "You won!", GREEN
+        else:
+            return True, "You lost...!", RED
+    return False, "", WHITE
+
+
+def game_loop(screen):
+    running = True
+    clock = pygame.time.Clock()
+    board = np.zeros(BOARD_SIZE, dtype=int)
+    is_player_move = True
+    game_ended = False
+    info_text = ""
+    text_color = WHITE
+    while running:
+        # do AI move
+        if not is_player_move:
+            # -1 is player, 1 is ai
+            ai_action = connect_ai.min_max_value(board)
+            board = connect_ai.apply_action(board, ai_action, 1)
+            game_ended, info_text, text_color = is_finished(board)
+            is_player_move = True
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            if event.type == pygame.MOUSEBUTTONUP:
+                # do player move
+                if is_player_move and not game_ended:
+                    action = pygame.mouse.get_pos()[0] // 100
+                    board = connect_ai.apply_action(board, action, -1)
+                    game_ended, info_text, text_color = is_finished(board)
+                    is_player_move = False
+
+                # reset for new game
+                if game_ended:
+                    board = np.zeros(BOARD_SIZE, dtype=int)
+                    is_player_move = True
+                    info_text = ""
+                    text_color = WHITE
+                    game_ended = False
+
+        screen.fill(WHITE)
+        for y in range(len(board)):
+            for x in range(len(board[0])):
+                color = BLACK
+                if board[y][x] == -1:
+                    color = GREEN
+                elif board[y][x] == 1:
+                    color = RED
+                pygame.draw.rect(
+                    screen, WHITE, [x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE], 0)
+                pygame.draw.rect(
+                    screen, color, [x * BLOCK_SIZE + BORDER_SIZE, y * BLOCK_SIZE + BORDER_SIZE, BLOCK_SIZE - 2 * BORDER_SIZE, BLOCK_SIZE - 2 * BORDER_SIZE], 0)
+
+        if info_text != "":
+            textsurface = GAME_FONT.render(info_text, False, text_color)
+            screen.blit(textsurface, (200, 200))
+        pygame.display.flip()
+
+    clock.tick(60)
+
+
+def main():
+    size = np.flip(BLOCK_SIZE * np.array(BOARD_SIZE))
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("Connect 4 - AI")
+    game_loop(screen)
+    pygame.quit()
+
+
+main()
